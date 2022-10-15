bf76155b64d5ad3a6eeaccb6943af483dea43961
EmperorAkashi <linchen4869@ucla.edu>
2022-10-14 22:27:11 -0700

    add camera model macro and classes

--- src/camera_model.cpp
+++ src/camera_model.cpp
@@ -1,10 +1,9 @@
 // manipulate all camera models and their index of parameters,
 // i.e. focal (x, y) and principal
-#include "base/camera_models.h"
+#include "camera_models.h"
 
 #include <unordered_map>
 
 std::unordered_map<std::string, int> InitialzeCameraModelNameToId() {
   std::unordered_map<std::string, int> camera_model_name_to_id;
 
-  
