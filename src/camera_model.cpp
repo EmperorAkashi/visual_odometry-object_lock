c6e3db3526787449c11071ff854cafebc32ec0e8
EmperorAkashi <linchen4869@ucla.edu>
2022-10-20 22:09:17 -0700

    update fxn of camera_model

--- src/camera_model.cpp
+++ src/camera_model.cpp
@@ -30,7 +30,7 @@ CAMERA_MODEL_CASES
 
 std::unordered_map<std::string, int> InitialzeCameraModelNameToId() {
   std::unordered_map<std::string, int> camera_model_name_to_id;
-//use macro to set cameramodel id & name
+//use macro to set cameramodel id & name??
 #define CAMERA_MODEL_CASE(CameraModel)                     \
   camera_model_name_to_id.emplace(CameraModel::model_name, \
                                   CameraModel::model_id);
@@ -40,6 +40,21 @@ std::unordered_map<std::string, int> InitialzeCameraModelNameToId() {
 #undef CAMERA_MODEL_CASE
 
   return camera_model_name_to_id;
+}
+
+//use macro to generate model's id and name then append it to map
+std::unordered_map<int, std::string> InitialzeCameraModelIdToName() {
+  std::unordered_map<int, std::string> camera_model_id_to_name;
+
+#define CAMERA_MODEL_CASE(CameraModel)                   \
+  camera_model_id_to_name.emplace(CameraModel::model_id, \
+                                  CameraModel::model_name);
+
+  CAMERA_MODEL_CASES
+
+#undef CAMERA_MODEL_CASE
+
+  return camera_model_id_to_name;
 }
 
 std::unordered_map<int, std::string> InitialzeCameraModelIdToName() {
@@ -54,4 +69,34 @@ std::unordered_map<int, std::string> InitialzeCameraModelIdToName() {
 #undef CAMERA_MODEL_CASE
 
   return camera_model_id_to_name;
+}
+
+//method that create generate a map of model to/ id
+static const std::unordered_map<std::string, int> CAMERA_MODEL_NAME_TO_ID =
+    InitialzeCameraModelNameToId();
+
+static const std::unordered_map<int, std::string> CAMERA_MODEL_ID_TO_NAME =
+    InitialzeCameraModelIdToName();
+
+bool ExistsCameraModelWithName(const std::string& model_name) {
+  return CAMERA_MODEL_NAME_TO_ID.count(model_name) > 0;
+}
+
+//create an iterator to search camera's model name
+int CameraModelNameToId(const std::string& model_name) {
+  const auto it = CAMERA_MODEL_NAME_TO_ID.find(model_name);
+  if (it == CAMERA_MODEL_NAME_TO_ID.end()) {
+    return kInvalidCameraModelId;
+  } else {
+    return it->second;
+  }
+}
+
+std::string CameraModelIdToName(const int model_id) {
+  const auto it = CAMERA_MODEL_ID_TO_NAME.find(model_id);
+  if (it == CAMERA_MODEL_ID_TO_NAME.end()) {
+    return "";
+  } else {
+    return it->second;
+  }
 }
