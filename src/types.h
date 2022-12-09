adee1b3b5c2d7418c74d07366bc5d4c4b57cc167
EmperorAkashi <linchen4869@ucla.edu>
2022-12-09 12:03:46 -0800

    change type and add alignment

--- src/types.h
+++ src/types.h
@@ -2,11 +2,32 @@
 #define SRC_TYPES_H_
 
 #include <cstdint>
+#include <Eigen/Core>
 
-// typedef int8_t uint8_t;
-// typedef int16_t uint16_t;
-// typedef int32_t uint32_t;
-// typedef int64_t uint64_t;
+#include "util/alignment.h"
+
+#ifdef _MSC_VER
+#if _MSC_VER >= 1600
+#include <cstdint>
+#else
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#endif
+#elif __GNUC__ >= 3
+#include <cstdint>
+#endif
+
+// Define non-copyable or non-movable classes.
+#define NON_COPYABLE(class_name)          \
+  class_name(class_name const&) = delete; \
+  void operator=(class_name const& obj) = delete;
+#define NON_MOVABLE(class_name) class_name(class_name&&) = delete;
 
 // Unique identifier for cameras.
 typedef uint32_t camera_t;
@@ -45,6 +66,20 @@ typedef Eigen::Matrix<uint8_t, 4, 1> Vector4ub;
 typedef Eigen::Matrix<double, 6, 1> Vector6d;
 
 }  // namespace Eigen
+
+namespace std {
+
+// Hash function specialization for uint32_t pairs, e.g., image_t or camera_t.
+template <>
+struct hash<std::pair<uint32_t, uint32_t>> {
+  std::size_t operator()(const std::pair<uint32_t, uint32_t>& p) const {
+    const uint64_t s = (static_cast<uint64_t>(p.first) << 32) +
+                       static_cast<uint64_t>(p.second);
+    return std::hash<uint64_t>()(s);
+  }
+};
+
+}  // namespace std
 
 
 #endif  // SRC_TYPES_H_
