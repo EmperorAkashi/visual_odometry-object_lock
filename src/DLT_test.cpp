362c7276a1d423965432308b935f88561e74b3e1
EmperorAkashi <linchen4869@ucla.edu>
2023-01-07 00:28:10 -0800

    manually def linear system from 6 control points

--- src/DLT_test.cpp
+++ src/DLT_test.cpp
@@ -23,9 +23,16 @@ Eigen::Matrix3x4d DLT(vector<Eigen::Vector3d> ControlPoints, vector<Eigen::Vecto
     {
         Point3D curr_3d = ControlPoints[i];
         Point2D curr_2d = ImagePoints[i];
-        vector<float> curr;
-        curr.push_back(curr_3d.X());
+        float x, y, z = curr_3d.X(), curr_3d.Y(), curr_3d.Z();
+        float u, v = curr_2d.X(), curr_2d.Y();
+        vector<float> curr_1{x, y, z, 1, 0, 0, 0, 0, -u*x, -u*y, -u*z, -u};
+        vector<float> curr_2{0, 0, 0, 0, x, y, z, 1, -v*x, -v*y, -v*z, -v}; //does the negative sign matters?
+        LinearSystem.push_back(curr_1);
+        LinearSystem.push_back(curr_2);
     }
+
+    int n_rows, n_cols = LinearSystem.size(), LinearSystem[0].size();
+    DLTMatrix M = Eigen::Map<DLTMatrix>(LinearSystem.data(), n_rows, n_cols);
     
 }
 
